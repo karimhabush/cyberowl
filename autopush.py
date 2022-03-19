1c01ac8fcd2bfe76ac6917c2c6b0e28e32119b43
karimhabush <karim.habush@gmail.com>
2022-03-19 21:01:17 +0100

    Fix error in autopush

--- autopush.py
+++ autopush.py
@@ -6,14 +6,15 @@ load_dotenv()
 GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
 GITHUB_USERNAME = os.environ.get("GITHUB_USERNAME")
 
+REMOTE = f"https://{GITHUB_USERNAME}:{GITHUB_ACCESS_TOKEN}@github.com/{GITHUB_ACCESS_TOKEN}/cyberowl.git"
+CLONE_TO = os.getcwd()
 try:
-    git.Git(os.getcwd()).clone(os.environ.get("GITHUB_REPO"))
+    git.Repo.clone_from(REMOTE, CLONE_TO)
 except Exception:
     print("[INFO]: Already cloned")
 
 # make sure .git folder is properly configured
 PATH_OF_GIT_REPO = os.getcwd() + r"/cyberowl/.git"
-print(PATH_OF_GIT_REPO)
 COMMIT_MESSAGE = 'This is a commit from heroku! time to merge hh !'
 FILE_TO_ADD = "README.md"
 BRANCH = "dev"
