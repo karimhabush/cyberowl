feb4b09fca3f036be90a16984e626acdd52a2e1a
karimhabush <karim.habush@gmail.com>
2022-03-19 13:36:24 +0100

    Add dummy unit tests

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from IBMcloudSpider import IBMCloudSpider
-from CISASpider import CisaSpider
-from CertFrSpider import CertFrSpider
-from DgssiSpider import DgssiSpider
-from ZDISpider import ZDISpider
+from spiders.IBMcloudSpider import IBMCloudSpider
+from spiders.CISASpider import CisaSpider
+from spiders.CertFrSpider import CertFrSpider
+from spiders.DgssiSpider import DgssiSpider
+from spiders.ZDISpider import ZDISpider
 from scrapy.crawler import CrawlerProcess
 from datetime import datetime
 from autopush import git_push
@@ -11,10 +11,10 @@ from autopush import git_push
 def main():
     now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-    item = f"""<div id="top"></div>\n\n ## CyberOwl \n > Last Updated {now} \n\n
-    A daily updated summary of the most frequent types of security incidents currently being reported from different sources.\n\n
-    ### Jump to \n * [CISA](#cisa-arrow_heading_up)\n* [MA-CERT](#ma-cert-arrow_heading_up)\n* [CERT-FR](#cert-fr-arrow_heading_up)
-    # \n* [IBMCLOUD](#ibmcloud-arrow_heading_up)\n* [ZeroDayInitiative](#zerodayinitiative-arrow_heading_up)\n\n"""
+    item = f"""<div id="top"></div>\n\n## CyberOwl \n> Last Updated {now} \n\n
+A daily updated summary of the most frequent types of security incidents currently being reported from different sources.\n\n
+### Jump to \n * [CISA](#cisa-arrow_heading_up)\n* [MA-CERT](#ma-cert-arrow_heading_up)\n* [CERT-FR](#cert-fr-arrow_heading_up)
+\n* [IBMCLOUD](#ibmcloud-arrow_heading_up)\n* [ZeroDayInitiative](#zerodayinitiative-arrow_heading_up)\n\n"""
 
     with open("README.md", "w") as f:
         f.write(item)
