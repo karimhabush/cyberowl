1fb66eec154725b7f1a4f93459df55d2e6d1df3d
Abdelkarim HABOUCH <karim.habush@gmail.com>
2023-01-06 12:12:10 +0100

    Change spiders names for ZDI and IBM

--- src/cyberowl/spiders/zdi_spider.py
+++ src/cyberowl/spiders/zdi_spider.py
@@ -28,7 +28,7 @@ class ZDISpider(scrapy.Spider):
         description_selector : The CSS/XPATH selector of the description of the alert.
     """
 
-    name = "ZeroDayInitiative"
+    name = "ZERODAYINITIATIVE"
     max_bulletins = 7
     start_urls = ["https://www.zerodayinitiative.com/advisories/published/"]
     block_selector = "descendant-or-self::table[contains(@class,'table')]/tbody/tr"
