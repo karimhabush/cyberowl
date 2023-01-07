df16fe1993e77095331a2966001cc8909a5077bd
karimhabush <karim.habush@gmail.com>
2023-01-07 19:49:11 +0100

    Change ZDI link to 2022's

--- src/cyberowl/spiders/zdi_spider.py
+++ src/cyberowl/spiders/zdi_spider.py
@@ -30,7 +30,7 @@ class ZDISpider(scrapy.Spider):
 
     name = "ZERODAYINITIATIVE"
     max_bulletins = 7
-    start_urls = ["https://www.zerodayinitiative.com/advisories/published/"]
+    start_urls = ["https://www.zerodayinitiative.com/advisories/published/2022/"]
     block_selector = "descendant-or-self::table[contains(@class,'table')]/tbody/tr"
     link_selector = ".//a"
     title_selector = ".//a"
