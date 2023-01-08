2fde681f64ce3a3f1d1b4ecda46213c6a46953a5
chaymae-jhr <chaymae.eljouhari@gmail.com>
2023-01-08 13:17:16 +0100

    Added source 'HK_CERT'

--- docs/activity/IBM-X-FORCE-EXCHANGE.md
+++ docs/activity/IBM-X-FORCE-EXCHANGE.md
@@ -1,1 +1,14 @@
-# IBM X-FORCE EXCHANGE
+
+
+# IBM-X-FORCE-EXCHANGE
+
+ |Title|Description|Date|
+ |---|---|---|
+ |[Apache James MIME4J information disclosure (CVE-2022-45787)](https://exchange.xforce.ibmcloud.com/activity/list?filter=Vulnerabilities)|Visit link for details|Jan 6, 2023|
+ |[Apache James server information disclosure (CVE-2022-45935)](https://exchange.xforce.ibmcloud.com/activity/list?filter=Vulnerabilities)|Visit link for details|Jan 6, 2023|
+ |[Zoom Rooms for Windows installers privilege escalation (CVE-2022-36930)](https://exchange.xforce.ibmcloud.com/activity/list?filter=Vulnerabilities)|Visit link for details|Jan 6, 2023|
+ |[Zoom Rooms for Windows clients privilege escalation (CVE-2022-36929)](https://exchange.xforce.ibmcloud.com/activity/list?filter=Vulnerabilities)|Visit link for details|Jan 6, 2023|
+ |[Zoom for Android clients directory traversal (CVE-2022-36928)](https://exchange.xforce.ibmcloud.com/activity/list?filter=Vulnerabilities)|Visit link for details|Jan 6, 2023|
+ |[Zoom Rooms for macOS privilege escalation (CVE-2022-36927)](https://exchange.xforce.ibmcloud.com/activity/list?filter=Vulnerabilities)|Visit link for details|Jan 6, 2023|
+ |[Zoom Rooms for macOS privilege escalation (CVE-2022-36926)](https://exchange.xforce.ibmcloud.com/activity/list?filter=Vulnerabilities)|Visit link for details|Jan 6, 2023|
+ 
---
+++ src/cyberowl/spiders/hk_cert_spider.py
@@ -0,0 +1,50 @@
+"""
+    This spider is used to scrape alerts from the following source:
+    https://www.hkcert.org/security-bulletin?item_per_page=10
+"""
+import scrapy
+from items import AlertItem
+
+
+class HKCERTSpider(scrapy.Spider):
+    """Spider for the Hong Kong CERT.
+
+    This spider is used to scrape data from the official website of
+    Hong Kong CERT.
+
+    Attributes:
+        name : Name of the spider.
+        max_items : The maximum number of items to scrape.
+        start_url : The website from which to start crawling.
+        block_selector : The CSS/XPATH selector of the block containing the data.
+        link_selector : The CSS/XPATH selector of the link of the alert.
+        title_selector : The CSS/XPATH selector of the title of the alert.
+        date_selector : The CSS/XPATH selector of the date of creation of the alert.
+        description_selector : The CSS/XPATH selector of the description of the alert.
+    """
+
+    name = "HK-CERT"
+    max_items = 10
+    start_urls = ["https://www.hkcert.org/security-bulletin?item_per_page=10"]
+    block_selector = "a.listingcard__item"
+    link_selector = "./@href"
+    title_selector = ".//p[contains(@class, 'listingcard__title')]//text()"
+    date_selector = ""
+    description_selector = ".//div[contains(@class, 'listingcard__desc')]//text()"
+
+    def parse(self, response):
+        """
+        Parsing the response
+        """
+        for idx, bulletin in enumerate(response.css(self.block_selector)):
+
+            if idx > self.max_items:
+                break
+            item = AlertItem()
+
+            item["title"] = bulletin.xpath(self.title_selector).get()
+            item["link"] = bulletin.xpath(self.link_selector).get()
+            item["date"] = "Visit websie for more details"
+            item["description"] = bulletin.xpath(self.description_selector).get()
+
+            yield item
