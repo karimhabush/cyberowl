a45b24ed50d995111701ed3a8a4c5033379919f9
karimhabush <karim.habush@gmail.com>
2023-01-15 11:53:01 +0100

    Add Canadian Center for Cyber Security

--- src/cyberowl/main.py
+++ src/cyberowl/main.py
@@ -1,14 +1,15 @@
 import settings as cyberowl_settings
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
+from spiders.ca_ccs_spider import CaCCSSpider
 from spiders.cert_fr_spider import CertFRSpider
 from spiders.cisa_spider import CisaSpider
+from spiders.hk_cert_spider import HKCERTSpider
 from spiders.ibmcloud_spider import IBMCloudSpider
 from spiders.ma_cert_spider import MACertSpider
 from spiders.vigilance_spider import VigilanceSpider
 from spiders.vuldb_spider import VulDBSpider
 from spiders.zdi_spider import ZDISpider
-from spiders.hk_cert_spider import HKCERTSpider
 from utils import generate_heading, generate_table_of_content, write_to_readme
 
 
@@ -25,14 +26,15 @@ def main():
     process = CrawlerProcess(settings=crawler_settings)
 
     spiders_cls = [
-        CisaSpider,
-        MACertSpider,
-        CertFRSpider,
-        IBMCloudSpider,
-        ZDISpider,
-        VigilanceSpider,
-        VulDBSpider,
-        HKCERTSpider
+        # CisaSpider,
+        # MACertSpider,
+        # CertFRSpider,
+        # IBMCloudSpider,
+        # ZDISpider,
+        # VigilanceSpider,
+        # VulDBSpider,
+        # HKCERTSpider,
+        CaCCSSpider,
     ]
 
     for spider in spiders_cls:
